e301fc8ccfc69dcdc93c076f675aa6e5fa7ac501
Ismaelp96 <ismael6991@live.com>
2022-12-04 09:34:39 -0300

    Refatorando promisse

--- promise1.js
+++ promise1.js
@@ -1,17 +1,10 @@
-function primeiroElemento(array) {
-  return array[0];
-}
-
-function primeiraLetra(string) {
-  return string[0];
-}
-
+const primeiroElemento = (string) => string[0];
 const letraMinuscula = (letra) => letra.toLowerCase();
 
-new Promise(function (cumprirPromessa) {
-  cumprirPromessa(['Ana', 'Bia', 'Carlos', 'Daniel']);
+new Promise(function (resolve) {
+  resolve(['Ana', 'Bia', 'Carlos', 'Daniel']);
 })
-  .then(primeiroElemento)
-  .then(primeiraLetra)
+  .then(primeiroElemento) // -> primeiro elemento do array
+  .then(primeiroElemento) // -> primeira letra
   .then(letraMinuscula)
   .then(console.log);
